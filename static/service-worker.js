d4c210872764e980a7fb3e40be6772b3b43d2598
virat2118 <your-email@example.com>
2026-02-15 19:54:57 +0530

    add service js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,25 +1,40 @@
-const CACHE_NAME = "ruchigo-v3";
+const CACHE_NAME = "ruchigo-v6";
+
+const STATIC_FILES = [
+  "/static/css/style.css",
+  "/static/images/logo4.png"
+];
 
 self.addEventListener("install", event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache =>
-      cache.addAll([
-        "/",
-        "/static/css/style.css"
-      ])
-    )
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_FILES))
   );
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+
+  if (event.request.url.includes("/static/")) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(res => res || fetch(event.request))
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
