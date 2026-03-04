006f4a7c65657e554c83777930f6cfe7e48c5487
virat2118 <your-email@example.com>
2026-03-05 01:42:49 +0530

    add firbse again for shwoing

--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -1,4 +1,6 @@
-/* ================= FIREBASE IMPORTS ================= */
+
+
+    /* ================= FIREBASE IMPORTS ================= */
 importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
 
