c6bed43558e7bbfa752b142e337b727e34f9a697
Koppadi Veera Manikanta <kopmanikanta2650@gmail.com>
2026-08-24 20:22:27 +0530

    add nbew versiuon

--- static/js/version.js
+++ static/js/version.js
@@ -4,8 +4,8 @@
 
 // Change ONLY these values on every deployment
 
-const APP_VERSION = "1.0.1";
-const BUILD_NUMBER = "20260713";
+const APP_VERSION = "1.0.2";
+const BUILD_NUMBER = "20260824";
 
 // Service Worker Cache Name
 const CACHE_NAME = `ruchigo-v${APP_VERSION}`;
