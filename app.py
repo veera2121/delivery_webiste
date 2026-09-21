11e245b0f7f5d85d7f8f1d7bb5bf69acadffbcc4
Koppadi Veera Manikanta <kopmanikanta2650@gmail.com>
2026-09-22 01:34:51 +0530

    add app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ from functools import wraps
 import firebase_admin
 from firebase_admin import credentials
 
-#from dispatch_service import haversine 
+from dispatch_service import haversine 
 from dispatch_service import assign_delivery_to_order,assign_waiting_order_to_rider,find_nearest_rider_excluding
 # ================= LOCAL IMPORTS =================
 from push import send_push
@@ -3320,7 +3320,7 @@ def home():
 
     return render_template(
 
-        "sameindex.html",
+        "index.html",
 
 
         # Stores
